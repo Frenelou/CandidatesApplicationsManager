3995c637b371108a84a9521508ec1d841286cf1c
Flora <flora.rene@gmail.com>
2022-01-14 02:33:50 +0100

    HashRouter for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { ChakraProvider, Box, theme } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./routes/Dashboard";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl" p={10}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Box>
   </ChakraProvider>
 );
